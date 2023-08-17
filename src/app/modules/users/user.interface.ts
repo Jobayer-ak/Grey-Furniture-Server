879e6b354dc0c15962970ce9d1a515d360fd4f7e
Jobayer_ak <jobayer.ak@gmail.com>
2023-08-18 00:10:29 +0600

    logger, global error, sendResponse CatchAsync function all are done

--- src/app/modules/users/user.interface.ts
+++ src/app/modules/users/user.interface.ts
@@ -1,4 +1,15 @@
-type IUser {
+export type UserName = {
+  firstName: string;
+  lastName: string;
+};
+
+export type IUser = {
+  name: UserName;
   role: string;
+  email: string;
   password: string;
-}
+  gender: 'Male' | 'Female';
+  mobile: string;
+  address?: string;
+  profileImage?: string;
+};
