d4db2633508ffa988671c97f78478ce174b19f0c
Jobayer_ak <jobayer.ak@gmail.com>
2023-08-19 16:00:14 +0600

    auth interface is done and working on auth controller service and routes

---
+++ src/app/modules/auth/auth.interface.ts
@@ -0,0 +1,14 @@
+export type ILoginUser = {
+  email: string;
+  password: string;
+};
+
+export type ILoginUserResponse = {
+  accessToken: string;
+  refreshToken?: string;
+  needsPasswordChange: boolean | undefined;
+};
+
+export type IRefreshTokenResponse = {
+  accessToken: string;
+};
